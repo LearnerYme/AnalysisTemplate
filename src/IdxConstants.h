5bdcc55c90ff4fcad8d1ddb7c53e45c704a06d23
Yme <yghuang@mails.ccnu.edu.cn>
2024-02-27 11:04:39 +0100

    v2.0 Index constants header: The definition of constants are moved to cxx file

--- src/IdxConstants.h
+++ src/IdxConstants.h
@@ -7,6 +7,9 @@
     No guarantee for higher or lower patch!!!
 */
 
+#ifndef __IDXCONSTANT_HEADER__
+#define __IDXCONSTANT_HEADER__
+
 // Note: this alias is called Indey instead of Index
 using Indey_t=int;
 
@@ -15,6 +18,7 @@ using Indey_t=int;
 #define __SIMPARTICLE_IDX__
 #define __MCEVENT_IDX__
 #define __RECEVENT_IDX__
+
 
 // VtxTracks
 #ifdef __VTXTRACK_IDX__
@@ -36,23 +40,6 @@ using Indey_t=int;
     extern const Indey_t IdxVtxNdf;
     extern const Indey_t IdxVtxNhits; // number of hits (total MVD+STS)
     extern const Indey_t IdxVtxCharge;
-
-    // init
-    constexpr Indey_t IdxVtxChi2 = 0;
-    constexpr Indey_t IdxVtxDcax = 2;
-    constexpr Indey_t IdxVtxDcay = 3;
-    constexpr Indey_t IdxVtxDcaz = 4;
-    constexpr Indey_t IdxVtxEta = -3; 
-    constexpr Indey_t IdxVtxP = -7;
-    constexpr Indey_t IdxVtxPt = -2;
-    constexpr Indey_t IdxVtxPx = -4;
-    constexpr Indey_t IdxVtxPy = -5;
-    constexpr Indey_t IdxVtxPz = -6;
-    constexpr Indey_t IdxVtxPhi = -1;
-    constexpr Indey_t IdxVtxVtxChi2 = 1;  
-    constexpr Indey_t IdxVtxNdf = 0;
-    constexpr Indey_t IdxVtxNhits = 2; // number of hits (total MVD+STS)
-    constexpr Indey_t IdxVtxCharge = 1;
 #endif
 
 // TofHits
@@ -65,15 +52,6 @@ using Indey_t=int;
     extern const Indey_t IdxTofMass2; // mass square
     extern const Indey_t IdxTofPhi;
     extern const Indey_t IdxTofRigi; // p * q, this maybe in lab frame, not cm frame
-
-    // init
-    constexpr Indey_t IdxTofDx = 4;
-    constexpr Indey_t IdxTofDy = 5;
-    constexpr Indey_t IdxTofDz = 6;
-    constexpr Indey_t IdxTofL = 1;
-    constexpr Indey_t IdxTofMass2 = 0;
-    constexpr Indey_t IdxTofPhi = -1;
-    constexpr Indey_t IdxTofRigi = 3;
 #endif
 
 // SimParticles
@@ -91,18 +69,6 @@ using Indey_t=int;
     // integer
     extern const Indey_t IdxSimMotherId; // -1 for primary
     extern const Indey_t IdxSimPdgId;
-
-    // init
-    constexpr Indey_t IdxSimEta = -6;
-    constexpr Indey_t IdxSimP = -10;
-    constexpr Indey_t IdxSimPt = -2;
-    constexpr Indey_t IdxSimPx = -7;
-    constexpr Indey_t IdxSimPy = -8;
-    constexpr Indey_t IdxSimPz = -9;
-    constexpr Indey_t IdxSimPhi = -1;
-    constexpr Indey_t IdxSimRap = -3;
-    constexpr Indey_t IdxSimMotherId = 0;
-    constexpr Indey_t IdxSimPdgId = -4;
 #endif
 
 // McEvent
@@ -112,12 +78,6 @@ using Indey_t=int;
     extern const Indey_t IdxMcVx;
     extern const Indey_t IdxMcVy;
     extern const Indey_t IdxMcVz;
-
-    // init
-    constexpr Indey_t IdxMcB = 1;
-    constexpr Indey_t IdxMcVx = -1;
-    constexpr Indey_t IdxMcVy = -2;
-    constexpr Indey_t IdxMcVz = -3;
 #endif
 
 // RecEvent
@@ -130,11 +90,6 @@ using Indey_t=int;
 
     // integer
     extern const Indey_t IdxRecMult; // total multiplicity in STS+MVD
+#endif
 
-    // init
-    constexpr Indey_t IdxRecVtxChi2 = 0;
-    constexpr Indey_t IdxRecVx = -1;
-    constexpr Indey_t IdxRecVy = -2;
-    constexpr Indey_t IdxRecVz = -3;
-    constexpr Indey_t IdxRecMult = 0;
 #endif
